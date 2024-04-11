dc8db3fa3d71750146d1a01d34fdc6c10714a121
Oursel06 <alexoursel6@gmail.com>
2024-04-11 11:49:38 +0200

    Connexion numéro de téléphone

--- App.js
+++ App.js
@@ -5,10 +5,14 @@ import { signup } from './auth_signup_password';
 import { signin } from './auth_signin_password';
 import { signwithgithub } from './auth_signin_popup';
 import Toast from 'react-native-root-toast';
+import { loginWithPhoneNumber } from './auth_phone_signin';
+import { verifyCode } from './auth_verify_code';
 
 export default function App() {
   const [email, onChangeEmail] = React.useState("");
   const [password, onChangePassword] = React.useState("");
+  const [phoneNumber, onChangePhoneNumber] = React.useState("");
+  const [code, onChangeCode] = React.useState("");
 
   const validateEmail = (email) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -50,6 +54,10 @@ export default function App() {
     signwithgithub()
   };
 
+  const handleSigninPhoneNumber = () => {
+    loginWithPhoneNumber(phoneNumber)
+  };
+
   return (
     <RootSiblingParent>
       <View style={styles.container}>
@@ -64,8 +72,22 @@ export default function App() {
           <Text style={styles.buttonText}>Connexion</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.button} onPress={handleSigninGitHub}>
-          <Text style={styles.buttonText}>Connexion avec GitHub</Text>
+        <TouchableOpacity style={styles.link} onPress={handleSigninGitHub}>
+          <Text style={styles.linkText}>Connexion avec GitHub</Text>
+        </TouchableOpacity>
+        <br></br>
+        <Text>Numéro de téléphone</Text>
+        <TextInput style={styles.input} onChangeText={onChangePhoneNumber} value={phoneNumber}></TextInput>
+        <TouchableOpacity style={styles.link} onPress={() => loginWithPhoneNumber(phoneNumber)}>
+          <Text style={styles.linkText}>Connexion avec téléphone</Text>
+        </TouchableOpacity>
+
+        <div id='recaptcha-container'></div>
+
+        <Text>code</Text>
+        <TextInput style={styles.input} onChangeText={onChangeCode} value={code}></TextInput>
+        <TouchableOpacity style={styles.button} onPress={() => verifyCode(code)}>
+          <Text style={styles.buttonText}>Vérifier le code</Text>
         </TouchableOpacity>
 
       </View>
@@ -98,8 +120,17 @@ const styles = StyleSheet.create({
     width: 200,
     margin: 5,
   },
+  link: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
+    margin: 5,
+  },
   buttonText: {
     color: '#fff',
+  },
+  linkText: {
+    color: 'blue',
   }
 });
 //test
